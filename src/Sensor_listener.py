12e219206dc927bcf680e1dbadb531118f70d7ab
Ondrej Vacek <ondrej.vacek2@tul.cz>
2020-04-13 21:18:12 +0200

    launch file add

--- src/Sensor_listener.py
+++ src/Sensor_listener.py
@@ -5,7 +5,7 @@ from ros_basics.msg import TempHumSensor
 
 
 def sensor_callback(sensor_msg):
-    rospy.loginfo("Recieved msg from: {}".format(sensor_msg.name))
+    rospy.loginfo("Recieved msg from: {} \t ID: {}".format(sensor_msg.name, sensor_msg.id))
     rospy.loginfo("Temperature: {} C".format(sensor_msg.temperature))
     rospy.loginfo("Humidity: {} %".format(sensor_msg.humidity))
 
@@ -16,3 +16,4 @@ if __name__ == "__main__":
     sensor_sub = rospy.Subscriber(sensor_topic, TempHumSensor, sensor_callback)
 
     rospy.spin()
+    
--- src/Sensor_publisher.py
+++ src/Sensor_publisher.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python
+import random
+import rospy
+from ros_basics.msg import TempHumSensor
+
+
+def talker():
+    rospy.init_node('Sensor_talker', anonymous=True)
+    pub = rospy.Publisher('/sensor', TempHumSensor) 
+
+    r = rospy.Rate(10)  # 10hz
+
+    msg = TempHumSensor()
+    msg.id = rospy.get_param("sensor_id")
+    msg.name = rospy.get_param("sensor_name")
+
+    while not rospy.is_shutdown():
+        msg.humidity = float(random.randint(3, 10))
+        msg.temperature =float(random.randint(3,10))
+        rospy.loginfo(msg)
+        pub.publish(msg)
+        r.sleep()
+
+if __name__ == '__main__':
+
+        try:
+            talker()
+        except rospy.ROSInterruptException:
+            pass
