c7dfdd37d6ff9579210e5dacbebded360581ac38
Ondrej Vacek <ondrej.vacek2@tul.cz>
2020-04-13 20:43:10 +0200

    sensro subscriber

--- src/Sensor_listener.py
+++ src/Sensor_listener.py
@@ -2,3 +2,17 @@
 
 import rospy
 from ros_basics.msg import TempHumSensor
+
+
+def sensor_callback(sensor_msg):
+    rospy.loginfo("Recieved msg from: {}".format(sensor_msg.name))
+    rospy.loginfo("Temperature: {} C".format(sensor_msg.temperature))
+    rospy.loginfo("Humidity: {} %".format(sensor_msg.humidity))
+
+if __name__ == "__main__":
+    rospy.init_node("Sensor_listener", anonymous=True)
+
+    sensor_topic = "/sensor"
+    sensor_sub = rospy.Subscriber(sensor_topic, TempHumSensor, sensor_callback)
+
+    rospy.spin()
